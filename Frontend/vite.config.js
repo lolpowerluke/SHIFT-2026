34e9162ff60865f773bb7a46ea812665770fd96b
JoachimGautama <joachim.gautama@student.ehb.be>
2026-02-10 17:11:22 +0100

    quicksave

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,6 +1,16 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import injectHTML from "vite-plugin-html-inject";
 
 export default defineConfig({
 	plugins: [injectHTML({ tagName: "load", sourceAttr: "file" })],
+	build: {
+		outdir: "dist",
+		rollupOptions: {
+			input: {
+				awards: resolve(__dirname, "awards/awards.html"),
+			},
+		},
+	},
+	base: "/pages",
 });
