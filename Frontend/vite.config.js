8430a8a2308d61736e215d431d5b0cd2ad9f94e3
JG-exe <joachim.gautama@gmail.com>
2026-02-12 00:36:10 +0100

    chore: update .gitignore, clean up package-lock.json, and format awards.html

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,14 +3,16 @@ import { defineConfig } from "vite";
 import injectHTML from "vite-plugin-html-inject";
 
 export default defineConfig({
-	plugins: [injectHTML({ tagName: "load", sourceAttr: "file" })],
-	build: {
-		outdir: "dist",
-		rollupOptions: {
-			input: {
-				awards: resolve(__dirname, "awards/awards.html"),
-			},
-		},
-	},
-	base: "/pages",
+  plugins: [injectHTML({ tagName: "load", sourceAttr: "file" })],
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        awards: resolve(__dirname, "pages/awards/awards.html"),
+        home: resolve(__dirname, "index.html"),
+      },
+    },
+  },
+  base: "/",
 });
