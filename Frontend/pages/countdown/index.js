20389630f82eba61328dd9cf8a5297ab3315144b
JG-exe <joachim.gautama@gmail.com>
2026-02-18 20:11:56 +0100

    feat: added form submission prevention if people do not agree to keep them up to date

--- Frontend/pages/countdown/index.js
+++ Frontend/pages/countdown/index.js
@@ -1,2 +1,12 @@
 // TODO: email submission
 // email can only be submitted if keep me up to date box is also checked
+
+const agree = document.getElementById("check");
+const submitButton = document.getElementById("submit");
+
+agree.addEventListener("change", function () {
+  submitButton.disabled = !this.checked;
+  console.log(this.checked, "click");
+  if (this.checked) submitButton.innerText = "→";
+  else submitButton.innerText = "x";
+});
