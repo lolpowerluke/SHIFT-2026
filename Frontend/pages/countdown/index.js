8761857d0e97c5311ead101140cd970919843c11
JG-exe <joachim.gautama@gmail.com>
2026-02-18 20:43:47 +0100

    feat: early return the submit function if the user didn't agree to the TOS

--- Frontend/pages/countdown/index.js
+++ Frontend/pages/countdown/index.js
@@ -1,11 +1,21 @@
 // TODO: email submission
+const API_URL = import.meta.env.VITE_API_URL;
 
 const agree = document.getElementById("check");
 const submitButton = document.getElementById("submit");
+const signupForm = document.getElementById("signup");
 
 agree.addEventListener("change", function () {
   submitButton.disabled = !this.checked;
   console.log(this.checked, "click");
   if (this.checked) submitButton.innerText = "→";
   else submitButton.innerText = "x";
+});
+
+signupForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  if (!agree.checked) return;
+  const email = Object.fromEntries(new FormData(signupForm));
+  //   const result = await fetch(`${API_URL}/mail/signup`);
+  console.log(email);
 });
