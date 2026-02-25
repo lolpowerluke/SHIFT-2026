a8df9d783efd9514cd31e04cd0cae2f305db7d26
JoachimGautama <joachim.gautama@student.ehb.be>
2026-02-25 11:25:06 +0100

    chore: clean up code structure and remove unused code blocks

--- Frontend/pages/countdown/index.js
+++ Frontend/pages/countdown/index.js
@@ -5,27 +5,31 @@ const submitButton = document.getElementById("submit");
 const signupForm = document.getElementById("signup");
 
 agree.addEventListener("change", function () {
-  submitButton.disabled = !this.checked;
-  console.log(this.checked, "click");
-  if (this.checked) submitButton.innerText = "→";
-  else submitButton.innerText = "x";
+	submitButton.disabled = !this.checked;
+	console.log(this.checked, "click");
+	if (this.checked) submitButton.innerText = "→";
+	else submitButton.innerText = "x";
 });
 
 signupForm.addEventListener("submit", async (e) => {
-  e.preventDefault();
-  if (!agree.checked) return;
+	e.preventDefault();
+	if (!agree.checked) return;
+	console.log("submit");
+	const { email } = Object.fromEntries(new FormData(signupForm));
 
-  const { email } = Object.fromEntries(new FormData(signupForm));
+	try {
+		const result = await fetch(`${API_URL}/mail/signup`, {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({ email }),
+		});
 
-  try {
-    const result = await fetch(`${API_URL}/mail/signup`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email }),
-    });
-
-    if (!result.ok) throw new Error(result.statusText);
-  } catch (e) {
-    console.error(e.message);
-  }
+		if (!result.ok) throw new Error(result.statusText);
+	} catch (e) {
+		console.error(e.message);
+	}
 });
+
+window.addEventListener("load", () =>
+	window.scrollTo({ top: 100, behavior: "smooth" }),
+);
