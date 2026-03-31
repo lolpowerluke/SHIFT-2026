e6ecfe2f84bb05f60ba185aca71cd072e3db5019
JG-exe <joachim.gautama@gmail.com>
2026-03-31 14:22:04 +0200

    chore: added documentation to Frontend/src/js/language.

--- Frontend/src/js/language.js
+++ Frontend/src/js/language.js
@@ -1,3 +1,6 @@
+// This changes the language of all links that have the .languageLink class if this file is linked in the html file. Before doing this, be sure that both links exist.
+// The links should all follow a strict structure `link/to/file/nl/`. Make sure to link them to the Dutch version by default
+
 getLanguage();
 
 function getLanguage() {
@@ -5,7 +8,7 @@ function getLanguage() {
   console.log(lang);
   if (!lang.includes("nl")) {
     document.querySelectorAll(".languageLink").forEach((link) => {
-      link.href = link.href.replace("nl", "en");
+      link.href = link.href.replace("/nl/", "/en/");
     });
   }
 }
