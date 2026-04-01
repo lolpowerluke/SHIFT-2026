03b2e57a1ac926b54e57665dfcb8a5b4befe3557
lolpowerluke <lukas.van.heddegem@proton.me>
2026-04-01 23:00:55 +0200

    CHORE: update database structure to include projects, proper ticket system and more user info

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -13,8 +13,62 @@ const db = mysql.createPool({
 await db.query(`
   CREATE TABLE IF NOT EXISTS users (
     id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    role ENUM('visitor', 'admin', '3rdyear', 'prof') NOT NULL,
     email VARCHAR(255) UNIQUE,
-    ticket VARCHAR(255) UNIQUE
+    password VARCHAR(255)
+  )
+`)
+
+await db.query(`
+  CREATE TABLE IF NOT EXISTS tickets (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    user BIGINT UNSIGNED,
+    code VARCHAR(255) UNIQUE,
+    CONSTRAINT fk_user
+      FOREIGN KEY(user)
+      REFERENCES users(id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
+  )
+`)
+
+await db.query(`
+  CREATE TABLE IF NOT EXISTS projects (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    description TEXT,
+    course ENUM('Website', 'Installatie', 'Mobile app', 'VR & AR', '3D games', 'Motion'),
+  )
+`)
+
+await db.query(`
+  CREATE TABLE IF NOT EXISTS project_user (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    user BIGINT UNSIGNED,
+    project BIGINT UNSIGNED,
+    CONSTRAINT fk_user
+      FOREIGN KEY(user)
+      REFERENCES users(id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
+    CONSTRAINT fk_project
+      FOREIGN KEY(project)
+      REFERENCES projects(id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
+  )
+`)
+
+await db.query(`
+  CREATE TABLE IF NOT EXISTS socials (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    user BIGINT UNSIGNED,
+    social VARCHAR(255) UNIQUE,
+    CONSTRAINT fk_user
+      FOREIGN KEY(user)
+      REFERENCES users(id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
   )
 `)
 
