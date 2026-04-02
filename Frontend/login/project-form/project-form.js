76f1c9671eac482e45ded0dda4e2454cdaa5fc37
NolanLamarque <nolan.lamarque@student.ehb.be>
2026-04-02 16:05:28 +0200

    Project form page for third years is done (except footer)

---
+++ Frontend/login/project-form/project-form.js
@@ -0,0 +1,41 @@
+
+/* Sources in README*/
+
+const choose_projectFile = document.getElementById("choose-projectFile");
+const choose_selfieFile = document.getElementById("choose-selfieFile");
+const imgPreview = document.getElementById("img-preview");
+const selfiePreview = document.getElementById("selfie-preview");
+
+
+choose_projectFile.addEventListener("change", function () {
+  getImgData();
+});
+
+choose_selfieFile.addEventListener("change", function () {
+  getSelfieData();
+});
+
+
+function getImgData() {
+  const files = choose_projectFile.files[0];
+  if (files) {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(files);
+    fileReader.addEventListener("load", function () {
+      imgPreview.style.display = "block";
+      imgPreview.innerHTML = '<img src="' + this.result + '" />';
+    });    
+  }
+}
+
+function getSelfieData() {
+  const files = choose_selfieFile.files[0];
+  if (files) {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(files);
+    fileReader.addEventListener("load", function () {
+      selfiePreview.style.display = "block";
+      selfiePreview.innerHTML = '<img src="' + this.result + '" />';
+    });    
+  }
+}
