adbc7497294dcf23fba9fb523f229f463b023942
JoachimGautama <joachim.gautama@student.ehb.be>
2026-04-04 17:55:54 +0200

    refactor: moved unused code to the bottom to keep an easier oversight over the used code

--- Frontend/pages/countdown/index.js
+++ Frontend/pages/countdown/index.js
@@ -6,32 +6,46 @@ const submitButton = document.getElementById("submit");
 const signupForm = document.getElementById("signup");
 
 agree.addEventListener("change", function () {
-  submitButton.disabled = !this.checked;
-  if (this.checked) submitButton.innerText = "→";
-  else submitButton.innerText = "x";
+	submitButton.disabled = !this.checked;
+	if (this.checked) submitButton.innerText = "→";
+	else submitButton.innerText = "x";
 });
 
 signupForm.addEventListener("submit", async (e) => {
-  e.preventDefault();
-  if (!agree.checked) return;
-  const { email } = Object.fromEntries(new FormData(signupForm));
+	e.preventDefault();
+	if (!agree.checked) return;
+	const { email } = Object.fromEntries(new FormData(signupForm));
 
-  try {
-    const result = await fetch(`${API_URL}/mail/signup`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email }),
-    });
+	try {
+		const result = await fetch(`${API_URL}/mail/signup`, {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({ email }),
+		});
 
-    if (!result.ok) throw new Error(result.statusText);
-  } catch (e) {
-    console.error(e.message);
-  }
+		if (!result.ok) throw new Error(result.statusText);
+	} catch (e) {
+		console.error(e.message);
+	}
 });
 
 window.addEventListener("load", () =>
-  window.scrollTo({ top: 100, behavior: "smooth" }),
+	window.scrollTo({ top: 100, behavior: "smooth" }),
 );
+
+const pageURL = "localhost:5173/pages/countdown/";
+document.getElementById("std").addEventListener("click", function () {
+	const isApple = /Mac|iPhone|iPad|iPod/.test(navigator.userAgent);
+	if (isApple) {
+		const link = document.createElement("a");
+		link.href = `webcal://${pageURL}/shiftCalendar.ics`;
+		link.click();
+	} else {
+		const gcalUrl = `https://calendar.google.com/calendar/render?action=TEMPLATE&text=SHIFT+-+Save+the+Date&dates=20260619T150000Z/20260619T190000Z&details=Our+third+year+students+are+showing+off+their+final+projects!&location=Nijverheidskaai+170,+1070+Anderlecht`;
+
+		window.open(gcalUrl, "_blank");
+	}
+});
 
 // document.getElementById("std").addEventListener("click", function () {
 //   const blob = new Blob([icsContent], { type: "text/calendar;charset=utf-8" });
@@ -44,16 +58,3 @@ window.addEventListener("load", () =>
 
 //   URL.revokeObjectURL(url);
 // });
-const pageURL = "localhost:5173/pages/countdown/";
-document.getElementById("std").addEventListener("click", function () {
-  const isApple = /Mac|iPhone|iPad|iPod/.test(navigator.userAgent);
-  if (isApple) {
-    const link = document.createElement("a");
-    link.href = `webcal://${pageURL}/shiftCalendar.ics`;
-    link.click();
-  } else {
-    const gcalUrl = `https://calendar.google.com/calendar/render?action=TEMPLATE&text=SHIFT+-+Save+the+Date&dates=20260619T150000Z/20260619T190000Z&details=Our+third+year+students+are+showing+off+their+final+projects!&location=Nijverheidskaai+170,+1070+Anderlecht`;
-
-    window.open(gcalUrl, "_blank");
-  }
-});
