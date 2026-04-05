142be98af24a503c45962cb9481244185749811a
JoachimGautama <joachim.gautama@student.ehb.be>
2026-04-05 19:08:40 +0200

    chore: added TODO comments

--- Frontend/pages/email_confirmation/index.js
+++ Frontend/pages/email_confirmation/index.js
@@ -6,7 +6,8 @@ const preConfirm = document.getElementById("preconfirmation");
 const postConfirm = document.getElementById("postconfirmation");
 const previouslyConfirmed = document.getElementById("previouslyconfirmed");
 
-// TODO: add backend check
+// TODO: check if works
+// TODO: add throbber while loading the backend check
 const swapContent = (result) => {
 	switch (result) {
 		case "success":
