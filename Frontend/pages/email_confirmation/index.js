cebc41d9445400ef8f8c025395f19ac64803f8ec
JoachimGautama <joachim.gautama@student.ehb.be>
2026-04-05 20:58:33 +0200

    feat: implemented error case

--- Frontend/pages/email_confirmation/index.js
+++ Frontend/pages/email_confirmation/index.js
@@ -5,34 +5,43 @@ const confirmBtn = document.getElementById("confirmBtn");
 const preConfirm = document.getElementById("preconfirmation");
 const postConfirm = document.getElementById("postconfirmation");
 const previouslyConfirmed = document.getElementById("previouslyconfirmed");
+const error = document.getElementById("error");
+const throbber = document.getElementById("throbber");
 
 // TODO: check if works
 // TODO: add throbber while loading the backend check
+
 const swapContent = (result) => {
 	switch (result) {
 		case "success":
 			preConfirm.classList.add("hidden");
 			postConfirm.classList.remove("hidden");
 			break;
-		case "previously confirmed":
+		case "confirmed":
 			preConfirm.classList.add("hidden");
 			previouslyConfirmed.classList.remove("hidden");
 			break;
 		case "error":
-			// todo: add smth went wrong
+			preConfirm.classList.add("hidden");
+			error.classList.remove("hidden");
 			break;
 		default:
 			console.warn(`unhandled case: ${result}`);
 	}
 };
 
-confirmBtn.addEventListener("click", () => {
+confirmBtn.addEventListener("click", async () => {
 	let response;
+	throbber.showModal();
 	try {
-		response = emailConfirmation();
+		response = await emailConfirmation();
 	} catch (e) {
-		console.error(e.message);
+		console.error("unsuccessful:", e.message);
+		response = "error";
+	} finally {
+		throbber.close();
 	}
+	swapContent(response);
 });
 
 async function emailConfirmation() {
