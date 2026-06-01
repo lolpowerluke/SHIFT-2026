587ec9c38d2f10335ebafb30b9e8162899afff39
JoachimGautama <joachim.gautama@student.ehb.be>
2026-06-01 17:38:21 +0200

    feat: changed .htaccess for React
    feat: added NavRoutes.js
    
    feat: added Router.jsx base setup

--- Frontend/src/Router.jsx
+++ Frontend/src/Router.jsx
@@ -1,9 +1,16 @@
 import {createBrowserRouter} from "react-router";
 import PageLayout from "./layouts/PageLayout.jsx";
 import BareLayout from "./layouts/BareLayout.jsx";
+import SearchLayout from "./layouts/SearchLayout.jsx";
 
 const router = createBrowserRouter(
     [
+        {
+            element: <SearchLayout/>,
+            children: [
+                {path: "/projects", element: "Projects"},
+            ]
+        },
         {
             element: <PageLayout/>,
             children: [
---
+++ Frontend/src/routes/constants/NavRoutes.js
@@ -0,0 +1,7 @@
+import Routes from "./Routes"
+import {NavLink} from "react-router";
+
+const NavRoutes = () => [
+    {route: Routes.Home, label: "Home"},
+    {route: Routes.Privacy, label: "Privacy"},
+]
