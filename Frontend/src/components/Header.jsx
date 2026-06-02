6024140185802d5a7d0a0d17a00e3fa8cedcbde1
JoachimGautama <joachim.gautama@student.ehb.be>
2026-06-02 11:06:00 +0200

    fix: add react-dom and fix comment

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -21,7 +21,7 @@ export default function Header() {
                             NavRoutes.map((link) => <NavLink key={link.route} to={link.route}>${link.label}</NavLink>)
                         }
                     </ul>
-                <!-- Code based on JoachimGautama's web2-course-project-front-end-JoachimGautama -->
+                // <!-- Code based on JoachimGautama's web2-course-project-front-end-JoachimGautama -->
                     <input type="checkbox" id="hamburgerNav"/>
                     <label htmlFor="hamburgerNav">
                         <div className="l1 line"></div>
