eb63af243fe624ecdccc338e5a765f8594af533e
YarnoVandenplas <Yarno.Vandenplas@student.ehb.be>
2026-06-03 15:31:10 +0200

    here comes the breaking

--- Frontend/src/Router.jsx
+++ Frontend/src/Router.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {createBrowserRouter} from "react-router";
+import { createBrowserRouter } from "react-router";
 import PageLayout from "./layouts/PageLayout.jsx";
 import BareLayout from "./layouts/BareLayout.jsx";
 import SearchLayout from "./layouts/SearchLayout.jsx";
@@ -8,33 +8,32 @@ import Routes from "./routes/constants/Routes.js";
 import PrivacyRedirect from "./pages/privacy/PrivacyRedirect.jsx";
 import PrivacyEN from "./pages/privacy/pages/PrivacyEN.jsx";
 import PrivacyNL from "./pages/privacy/pages/PrivacyNL.jsx";
+import Project from "./pages/Login/Login.jsx";
 
 // TODO: add actual page elements instead of placeholder strings
-const router = createBrowserRouter(
-    [
-        {
-            element: <PageLayout/>,
-            children: [
-                {path: Routes.Privacy, element: <PrivacyRedirect/>,
-                children: [
-                    {index: true, element: <PrivacyRedirect/>},
-                    {path: Routes.privacyEN, element: <PrivacyEN/>},
-                    {path: Routes.privacyNL, element: <PrivacyNL/>},
-                ]},
-                {path: Routes.Login3e, element: "Login3e"},
-                {
-                    element: <SearchLayout/>,
-                    children: [
-                        {path: "/projects", element: "Projects"},
-                    ]
-                },
-            ]
-        },
-        {
-            element: <BareLayout/>, children: [
-                {path: Routes.Root, element: <Countdown/>},
-            ]
-        }
-    ]
-)
+const router = createBrowserRouter([
+	{
+		element: <PageLayout />,
+		children: [
+			{
+				path: Routes.Privacy,
+				element: <PrivacyRedirect />,
+				children: [
+					{ index: true, element: <PrivacyRedirect /> },
+					{ path: Routes.privacyEN, element: <PrivacyEN /> },
+					{ path: Routes.privacyNL, element: <PrivacyNL /> },
+				],
+			},
+			{ path: Routes.Login, element: <Login /> },
+			{
+				element: <SearchLayout />,
+				children: [{ path: "/projects", element: "Projects" }],
+            },
+		],
+	},
+	{
+		element: <BareLayout />,
+		children: [{ path: Routes.Root, element: <Countdown /> }],
+	},
+]);
 export default router;
--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,38 +1,37 @@
-import {Link, NavLink} from "react-router";
-import {NavRoutes} from "../routes/constants/NavRoutes.js";
-
+import React from "react";
+import { Link, NavLink } from "react-router";
+import { NavRoutes } from "../routes/NavRoutes.js";
 
 export default function Header() {
-    return (
-        <header>
-            <script type="module" src="/src/js/language.js"></script>
-            <div className="ctx flexSpaceBetween">
-                <Link to="/">
-                    <img
-                        src="/favicon/shift_icon.svg"
-                        width="90"
-                        alt="Shift Icon"
-                        className="headerLogo"
-                    />
-                </Link>
-                <div className="nav">
-                    <ul>
-                        {
-                            NavRoutes.map((link) => <NavLink key={link.route} to={link.route}>
-                                <li>{link.label}</li>
-                            </NavLink>)
-                        }
-                    </ul>
-                {/* Code based on JoachimGautama's web2-course-project-front-end-JoachimGautama */}
-                    <input type="checkbox" id="hamburgerNav"/>
-                    <label htmlFor="hamburgerNav">
-                        <div className="l1 line"></div>
-                        <div className="l2 line"></div>
-                        <div className="l3 line"></div>
-                    </label>
-                </div>
-            </div>
-        </header>
-
-    )
+	return (
+		<header>
+			<script type="module" src="/src/js/language.js"></script>
+			<div className="ctx flexSpaceBetween">
+				<Link to="/">
+					<img
+						src="/favicon/shift_icon.svg"
+						width="90"
+						alt="Shift Icon"
+						className="headerLogo"
+					/>
+				</Link>
+				<div className="nav">
+					<ul>
+						{NavRoutes.map((link) => (
+							<NavLink key={link.route} to={link.route}>
+								<li>{link.label}</li>
+							</NavLink>
+						))}
+					</ul>
+					{/* Code based on JoachimGautama's web2-course-project-front-end-JoachimGautama */}
+					<input type="checkbox" id="hamburgerNav" />
+					<label htmlFor="hamburgerNav">
+						<div className="l1 line"></div>
+						<div className="l2 line"></div>
+						<div className="l3 line"></div>
+					</label>
+				</div>
+			</div>
+		</header>
+	);
 }
