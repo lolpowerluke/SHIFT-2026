f29d7968476a042b20994c50019ddd4c3352a604
Raicus <hamza.el.aisati@student.ehb.be>
2026-06-05 01:04:57 +0200

    feat: new route aangemaakt voor projecten

--- Frontend/src/routes/constants/Routes.js
+++ Frontend/src/routes/constants/Routes.js
@@ -1,11 +1,12 @@
 const Routes = {
-    Root: "",
-    Privacy: "/privacy",
-    privacyEN: "/privacy/en",
-    privacyNL: "/privacy/nl",
-    CountDown: "/countdown",
-    Project: "/project",
-    Login: "/login",
-    ProjectForm: "/project-form"
-}
+	Root: "",
+	Privacy: "/privacy",
+	privacyEN: "/privacy/en",
+	privacyNL: "/privacy/nl",
+	CountDown: "/countdown",
+	Project: "/project",
+	Login: "/login",
+	ProjectForm: "/project-form",
+	ProjectenPage: "/projectenpagina",
+};
 export default Routes;
