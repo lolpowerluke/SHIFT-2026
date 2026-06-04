bb4f1c619ab9e2ff028ca08849503d02dcd40d8e
lolpowerluke <lukas.van.heddegem@proton.me>
2026-06-04 14:57:45 +0200

    fix route v2

--- Frontend/src/Router.jsx
+++ Frontend/src/Router.jsx
@@ -26,7 +26,7 @@ const router = createBrowserRouter(
                     ]
                 },
                 { path: Routes.Root, element: <Countdown /> },
-                { path: Routes.Login, element: "Login3e" },
+                { path: Routes.Login, element: <Login /> },
                 { path: Routes.uMoeder, element: "isDik" }, //same as {path: "/uMoeder", element: "isDik"
                 {
                     element: <SearchLayout />, //this will hold all search logic for the projects
