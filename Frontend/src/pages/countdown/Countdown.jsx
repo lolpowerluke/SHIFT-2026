9078a861267406528c69d4ec921c4411b83ce81e
YarnoVandenplas <Yarno.Vandenplas@student.ehb.be>
2026-06-05 04:06:34 +0200

    fixed timer placement + removed unneeded copy

--- Frontend/src/pages/countdown/Countdown.jsx
+++ Frontend/src/pages/countdown/Countdown.jsx
@@ -1,167 +1,116 @@
 import React from "react";
 import "./index.css";
 import "../../css/style.css";
-import {useCountdown} from "../../js/countdown.js";
+import { useCountdown } from "../../js/countdown.js";
 
 export default function Countdown() {
-    const pageURL = "shiftfestival.be";
+	const pageURL = "shiftfestival.be";
 
-    function handleSTDClick() {
-        const isApple = /Mac|iPhone|iPad|iPod/.test(navigator.userAgent);
-        if (isApple) {
-            window.location.href = `webcal://${pageURL}/shiftCalendar.ics`;
-        } else {
-            const gcalUrl = `https://calendar.google.com/calendar/render?action=TEMPLATE&text=SHIFT+-+Save+the+Date&dates=20260619T150000Z/20260619T190000Z&details=Our+third+year+students+are+showing+off+their+final+projects!&location=Nijverheidskaai+170,+1070+Anderlecht`;
+	function handleSTDClick() {
+		const isApple = /Mac|iPhone|iPad|iPod/.test(navigator.userAgent);
+		if (isApple) {
+			window.location.href = `webcal://${pageURL}/shiftCalendar.ics`;
+		} else {
+			const gcalUrl = `https://calendar.google.com/calendar/render?action=TEMPLATE&text=SHIFT+-+Save+the+Date&dates=20260619T150000Z/20260619T190000Z&details=Our+third+year+students+are+showing+off+their+final+projects!&location=Nijverheidskaai+170,+1070+Anderlecht`;
 
-            window.open(gcalUrl, "_blank");
-        }
-    }
+			window.open(gcalUrl, "_blank");
+		}
+	}
 
-    const {timeLeft, blinkingS} = useCountdown()
+	const { timeLeft, blinkingS } = useCountdown();
 
-    return (
-        <>
-            <div className="heroContent">
-                <video autoPlay muted loop playsInline>
-                    <source src="/assets/heroContent/videohero_test.webm" type="video/webm"/>
-                    <img src="/assets/heroContent/heroImg.jpg" alt="EhB SHIFT festival hero image"/>
-                </video>
-            </div>
-            <div className="landingWrap">
-                <div>
-                    <div className="heroLayout">
-                        <div>
-                            <img
-                                src="../../assets/logos/shift_logo.svg"
-                                id="heroLogo"
-                                fetchPriority="high"
-                            />
-                        </div>
-                        <div>
-                            <div className="heroWrapper">
-                                <div className="rotatedText">
-                                    <h1>Eindprojecten</h1>
-                                    <div className="heroText">
-                                        <div className="courseHeroText">
-                                            <h1>
-                                                OPLEIDING MULTIMEDIA
-                                                <br/>
-                                                CREATIEVE TECHNOLOGIE
-                                            </h1>
-                                        </div>
-                                        <div>&</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="timerDiv xlarge">
-                    {timeLeft ? (<>
-                            <div className="cd darkText">
-                                <span>We tellen af!</span>
-                            </div>
-                            <div id="timer">
-                                <div className="timerBox">
-                                    <span>{timeLeft.days}</span>
-                                    <span>Dagen</span>
-                                </div>
-                                <div className="timerBox">
-                                    <span>{timeLeft.hours}</span>
-                                    <span>Uren</span>
-                                </div>
-                                <div className="timerBox">
-                                    <span>{timeLeft.minutes}</span>
-                                    <span>Minuten</span>
-                                </div>
-                                <div className="timerBox">
+	return (
+		<>
+			<div className="heroContent">
+				<video autoPlay muted loop playsInline>
+					<source
+						src="/assets/heroContent/videohero_test.webm"
+						type="video/webm"
+					/>
+					<img
+						src="/assets/heroContent/heroImg.jpg"
+						alt="EhB SHIFT festival hero image"
+					/>
+				</video>
+			</div>
+			<div className="landingWrap">
+				<div>
+					<div className="heroLayout">
+						<div>
+							<img
+								src="../../assets/logos/shift_logo.svg"
+								id="heroLogo"
+								fetchPriority="high"
+							/>
+						</div>
+						<div>
+							<div className="heroWrapper">
+								<div className="rotatedText">
+									<h1>Eindprojecten</h1>
+									<div className="heroText">
+										<div className="courseHeroText">
+											<h1>
+												OPLEIDING MULTIMEDIA
+												<br />
+												CREATIEVE TECHNOLOGIE
+											</h1>
+										</div>
+										<div>&</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="timerDiv xlarge">
+					{timeLeft ? (
+						<>
+							<div className="cd">
+								<span>We tellen af!</span>
+							</div>
+							<div id="timer">
+								<div className="timerBox">
+									<span className="darkText">{timeLeft.days}</span>
+									<span>Dagen</span>
+								</div>
+								<div className="timerBox">
+									<span className="darkText">{timeLeft.hours}</span>
+									<span>Uren</span>
+								</div>
+								<div className="timerBox">
+									<span className="darkText">{timeLeft.minutes}</span>
+									<span>Minuten</span>
+								</div>
+								<div className="timerBox">
 									<span>
-										<span className={blinkingS === "seconds" ? "hidden" : ""}>
+										<span
+											className={
+												blinkingS === "seconds" ? "hidden" : "darkText"
+											}
+										>
 											{timeLeft.seconds}
 										</span>
 									</span>
-                                    <span>Seconden</span>
-                                </div>
-                            </div>
-                            <br/>
-                            <div className="cta">
-                                <a
-                                    href="https://www.erasmushogeschool.be/nl/evenementen/shiftfestival"
-                                    className="linkBtn"
-                                    target="_blank">
-                                    Schrijf je nu gratis in!
-                                </a>
-                            </div>
-                        </>
-                    ) : (
-                        "LIVE NOW!"
-                    )}
-                </div>
-            </div>
-            {/*TODO: J past aan tot hier*/}
-            <div className="wrap topSpacer flexSpaceBetween">
-                <div className="section">
-                    <div className="main">
-                        <p>VRIJDAG 19 JUNI</p>
-                        <p>17:00 - 21:30</p>
-                    </div>
-                </div>
-                <div className="copy-text section darkText" id="copy-text">
-                    <div className="shiftCopy cardDiv">
-                        <h2>SHIFT Festival 2026</h2>
-                        <p>
-                            SHIFT FESTIVAL is een jaarlijks evenement georganiseerd door{" "}
-                            <a href="https://www.erasmushogeschool.be/nl">
-                                Erasmushogeschool Brussel
-                            </a>{" "}
-                            en de opleiding{" "}
-                            <a href="https://www.erasmushogeschool.be/nl/opleidingen/multimedia-en-creatieve-technologie">
-                                Multimedia & Creatieve Technologie
-                            </a>
-                            .
-                        </p>
-                        <br/>
-                        <p>
-                            Kom de indrukwekkende eindprojecten van de derdejaars bewonderen
-                            en breng je stem uit op je favoriete projecten, zodat ze tijdens
-                            de awardshow een prijs kunnen winnen.
-                        </p>
-                    </div>
-                    <div className="mctCopy cardDiv">
-                        <h2>Wat is MCT?</h2>
-                        <p>
-                            <a href="https://www.erasmushogeschool.be/nl/opleidingen/multimedia-en-creatieve-technologie">
-                                Multimedia & creatieve technologie
-                            </a>{" "}
-                            is een professionele bacheloropleiding aan de Erasmushogeschool
-                            Brussel.
-                            <br/>
-                            <br/>
-                            Deze opleiding combineert creativiteit met techniek, gericht op
-                            het ontwikkelen van digitale ervaringen, interactieve
-                            toepassingen, webtechnologieën en creatieve IT-oplossingen.
-                        </p>
-                    </div>
-                </div>
-                <div>
-                    <div>
-                        <p className="xlarge bold">
-                            Wil je er zeker bij zijn?
-                            <br/>
-                            Plaats het nu al in je agenda!
-                        </p>
-                    </div>
-                    <br/>
-                    <a
-                        className="linkBtn"
-                        id="std"
-                        onClick={handleSTDClick}
-                        title="Add this event to your calendar"
-                    >
-                        Save the date!
-                    </a>
-                </div>
-            </div>
-        </>
-    );
+									<span>Seconden</span>
+								</div>
+							</div>
+							<div className="cta section">
+								<a
+									href="https://www.erasmushogeschool.be/nl/evenementen/shiftfestival"
+									className="linkBtn"
+									target="_blank"
+								>
+									Schrijf je nu gratis in!
+								</a>
+							</div>
+						</>
+					) : (
+						"LIVE NOW!"
+					)}
+				</div>
+			</div>
+			{/*TODO: J past aan tot hier*/}
+			<div className="wrap topSpacer flexSpaceBetween"></div>
+		</>
+	);
 }
