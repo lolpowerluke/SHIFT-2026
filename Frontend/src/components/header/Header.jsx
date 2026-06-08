ac06847898a51bad94cb5986e05087f3535a6571
JoachimGautama <joachim.gautama@student.ehb.be>
2026-06-08 14:25:31 +0200

    fix header crap

--- Frontend/src/components/header/Header.jsx
+++ Frontend/src/components/header/Header.jsx
@@ -28,7 +28,7 @@ export default function Header() {
                         className={`${s.headerLogo} ${!scrolled ? s.hide : ""}`}
                     />
                 </Link>
-                <div className={`${s.nav} ${navLength <= 1 ? "hideThisSht" : "nope"}`}>
+                <div className={`${s.nav} ${navLength <= 1 ? "hideThisSht" : "n"}`}>
                     <ul className={s.navList}>
                         <ul className={s.orientationSwap}>
                             <li className={s.navLogo}>
