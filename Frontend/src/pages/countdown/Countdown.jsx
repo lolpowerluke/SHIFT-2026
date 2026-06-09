c9de418d6d0ddf2be6cdc8ebc45342021f7b8cd7
YarnoVandenplas <Yarno.Vandenplas@student.ehb.be>
2026-06-10 01:05:06 +0200

    dont forget the front

--- Frontend/src/pages/countdown/Countdown.jsx
+++ Frontend/src/pages/countdown/Countdown.jsx
@@ -31,24 +31,10 @@ export default function Countdown() {
 					<img
 						src="/assets/heroContent/heroImg.jpg"
 						alt="EhB SHIFT festival hero image"
+						media="(min-width: 1023px)"
 					/>
 				</video>
 			</div>
-    return (
-        <>
-            <div className={s.heroContent}>
-                <video autoPlay muted loop playsInline>
-                    <source src="/assets/heroContent/videoheroMobile.webm" media="(max-width: 1024px)"
-                            type="video/webm"/>
-                    <source media="(min-width: 1023px)" src="/assets/heroContent/videohero.webm"
-                            type="video/webm"/>
-                    <img
-                        src="/assets/heroContent/heroImg.jpg"
-                        alt="EhB SHIFT festival hero image"
-                        media="(min-width: 1023px)"
-                    />
-                </video>
-            </div>
 
 			<div className={s.landingWrap}>
 				<div>
@@ -136,80 +122,77 @@ export default function Countdown() {
 				</a>
 			</div>
 
-            <div
-                id="experience"
-                className={`${s.wrap} wrap ${s.previewSection}`}
-            >
-                <div className={s.section}>
-                    <div className={s.sectionFlexer}>
-                        <div className={s.projectSide}>
-                            <img src="/assets/sfeerbeeld.png" alt="sfeerbeeld"/>
-                            {/*<Carousel />*/}
-                        </div>
-                        <div className={s.shiftInfoSide}>
-                            <div className={s.shiftInfoNoCard}>
-                                <h2 className={s.infoTitle}>SHIFT festival</h2>
-                                <p>
-                                    SHIFT festival is het afstudeer-evenement van de opleiding
-                                    Multimedia & Creative Technoloiey aan Erasmus Hogeschool
-                                    Brussel. Ontdek er de eindprojecten van studenten, waarin
-                                    technologie, creativiteit en innovatie samenkomen. Een unieke
-                                    blik op het werk van de nieuwe generatie digitale makers!
-                                    <br/>
-                                    <br/>
-                                    Tijdens het festival kunnen projecten niet alleen bekeken,
-                                    maar ook getest en beleefd worden via demo’s, presentaties en
-                                    gesprekken met de makers. Daarnaast is er ruimte om te
-                                    netwerken en worden de meest opvallende projecten in de kijker
-                                    gezet. Met live awardshow!
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="altBg">
-                <div className={`section wrap ${s.wrap} ${s.infoSection}`}>
-                    <div>
-                        <h2>WAAR EN WANNEER?</h2>
-                        <div className={s.infoPart}>
-                            <div className={s.rightSideInfo}>
-                                <div className={s.shiftTime}>
-                                    <div className={s.iconCalendar}>
-                                        <img src="/assets/icons/CalendarBlue.svg" alt="Kalender"/>
-                                    </div>
-                                    <div>
-                                        <h3>VRIJDAG 19 JUNI</h3>
-                                        <p>17:00 - 21:30</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <hr className={s.sectionDivider}/>
-                            <div className={s.leftSideInfo}>
-                                <div className={s.shiftLocation}>
-                                    <div className={s.iconMap}>
-                                        <img src="/assets/icons/LocationBlue.svg" alt="Locatie"/>
-                                    </div>
-                                    <div>
-                                        <h3>ERASMUS HOGESCHOOL, CAMPUS KAAI</h3>
-                                        <button className={s.btnMaps} onClick={handleOpenMaps}>
-                                            Open in maps
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <h2>VOOR WIE?</h2>
-                        <div className={s.audienceContainer}>
-                            <div className={s.audienceCard}>
-                                <div className={s.iconHeadphones}>
-                                    <img src="/assets/icons/koptelefoon.svg" alt="Studenten"/>
-                                </div>
-                                <h3>STUDENTEN</h3>
-                                <p>Toon je werk aan honderden bezoekers en professionals.</p>
-                            </div>
+			<div id="experience" className={`${s.wrap} wrap ${s.previewSection}`}>
+				<div className={s.section}>
+					<div className={s.sectionFlexer}>
+						<div className={s.projectSide}>
+							<img src="/assets/sfeerbeeld.png" alt="sfeerbeeld" />
+							{/*<Carousel />*/}
+						</div>
+						<div className={s.shiftInfoSide}>
+							<div className={s.shiftInfoNoCard}>
+								<h2 className={s.infoTitle}>SHIFT festival</h2>
+								<p>
+									SHIFT festival is het afstudeer-evenement van de opleiding
+									Multimedia & Creative Technoloiey aan Erasmus Hogeschool
+									Brussel. Ontdek er de eindprojecten van studenten, waarin
+									technologie, creativiteit en innovatie samenkomen. Een unieke
+									blik op het werk van de nieuwe generatie digitale makers!
+									<br />
+									<br />
+									Tijdens het festival kunnen projecten niet alleen bekeken,
+									maar ook getest en beleefd worden via demo’s, presentaties en
+									gesprekken met de makers. Daarnaast is er ruimte om te
+									netwerken en worden de meest opvallende projecten in de kijker
+									gezet. Met live awardshow!
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="altBg">
+				<div className={`section wrap ${s.wrap} ${s.infoSection}`}>
+					<div>
+						<h2>WAAR EN WANNEER?</h2>
+						<div className={s.infoPart}>
+							<div className={s.rightSideInfo}>
+								<div className={s.shiftTime}>
+									<div className={s.iconCalendar}>
+										<img src="/assets/icons/CalendarBlue.svg" alt="Kalender" />
+									</div>
+									<div>
+										<h3>VRIJDAG 19 JUNI</h3>
+										<p>17:00 - 21:30</p>
+									</div>
+								</div>
+							</div>
+							<hr className={s.sectionDivider} />
+							<div className={s.leftSideInfo}>
+								<div className={s.shiftLocation}>
+									<div className={s.iconMap}>
+										<img src="/assets/icons/LocationBlue.svg" alt="Locatie" />
+									</div>
+									<div>
+										<h3>ERASMUS HOGESCHOOL, CAMPUS KAAI</h3>
+										<button className={s.btnMaps} onClick={handleOpenMaps}>
+											Open in maps
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div>
+						<h2>VOOR WIE?</h2>
+						<div className={s.audienceContainer}>
+							<div className={s.audienceCard}>
+								<div className={s.iconHeadphones}>
+									<img src="/assets/icons/koptelefoon.svg" alt="Studenten" />
+								</div>
+								<h3>STUDENTEN</h3>
+								<p>Toon je werk aan honderden bezoekers en professionals.</p>
+							</div>
 
 							<div className={s.audienceCard}>
 								<div className={s.iconPaper}>
@@ -226,7 +209,6 @@ export default function Countdown() {
 								<h3>tech-lovers</h3>
 								<p>Ontdek. Test. Laat je verrassen.</p>
 							</div>
-
 							<div className={s.audienceCard}>
 								<div className={s.iconStar}>
 									<img src="/assets/icons/star.svg" alt="Stemmen" />
@@ -238,6 +220,7 @@ export default function Countdown() {
 					</div>
 				</div>
 			</div>
+			<br />
 			<div className={`${s.infoMultimediaCard} section`}>
 				<div className={s.textContainer}>
 					<div>
@@ -266,44 +249,4 @@ export default function Countdown() {
 			</div>
 		</>
 	);
-                            <div className={s.audienceCard}>
-                                <div className={s.iconStar}>
-                                    <img src="/assets/icons/star.svg" alt="Stemmen"/>
-                                </div>
-                                <h3>bezoekers</h3>
-                                <p>Stem mee en bepaal de publiekswinnaar.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <br/>
-            <div className={`${s.infoMultimediaCard} section`}>
-                <div className={s.textContainer}>
-                    <div>
-                        <h2>WAT IS MULTIMEDIA & CREATIEVE TECHNOLOGIE?</h2>
-                        <p>
-                            Multimedia & Creatieve Technologie is een Bacheloropleiding op{" "}
-                            Erasmus Hogeschool Brussel. Hier komen design en technologie samen
-                            om digitale ervaringen te bouwen.
-                        </p>
-                    </div>
-                </div>
-                <img
-                    src="/assets/opleidingPlaceholder.jpg"
-                    alt="Multimedia & Creatieve Technologie"
-                />
-                <div className={`${s.cta}`}>
-                    <a
-                        href="https://www.erasmushogeschool.be/en/programmes/multimedia-and-creative-technology"
-                        className={`${s.linkBtn} linkBtn`}
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        Ontdek de richting!
-                    </a>
-                </div>
-            </div>
-        </>
-    );
 }
