810e9ba7ecb42d2c815488cc73b717a13cf15fc2
Hamza El Aisati <hamza.el.aisati@student.ehb.be>
2026-06-11 11:27:28 +0200

    style: added hero image/text/icon and button for the hero section of infopage + css (not finish yet)

--- Frontend/src/pages/info/Info.jsx
+++ Frontend/src/pages/info/Info.jsx
@@ -1,5 +1,28 @@
 import s from "./info.module.css";
 
 export default function Info() {
-	return <>Info</>;
+	return (
+		<>
+			<div className={s.heroContent}>
+				<img src="/assets/heroContent/infoHero.jpg" alt="" aria-hidden="true" />
+			</div>
+
+			<div className={s.heroLayout}>
+				<h1 className={s.heroTitle}>
+					Praktische <br /> Info
+					<img
+						src="/assets/icons/LocationOrange.svg"
+						alt=""
+						aria-hidden="true"
+						className={s.locationIcon}
+					/>
+				</h1>
+				<a href="#programma" className={`linkBtn ${s.heroBtn}`}>
+					Bekijk het programma
+				</a>
+			</div>
+
+			<main className="ctx"></main>
+		</>
+	);
 }
