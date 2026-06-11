4ee8326af29c7cac87b1fb4d3e545d58014fd8f2
JoachimGautama <joachim.gautama@student.ehb.be>
2026-06-11 15:43:15 +0200

    update 404 page

--- Frontend/src/pages/error/NotFound.jsx
+++ Frontend/src/pages/error/NotFound.jsx
@@ -4,11 +4,11 @@ import Routes from "../../routes/constants/Routes";
 const NotFound = () => {
     return (
         <>
-            <div className="ctx flexCenter">
-                <h1>404 - Not Found</h1>
-                <p>This is not the page you are looking for.</p>
-                <Link to={Routes.Root} className="linkBtn">Go to Homepage</Link>
-                <img src="/assets/404.webp" alt="404 img"/>
+            <div className="ctx flexCenter headerSpacer">
+                    <h1>404 - Not Found</h1>
+                    <p>This is not the page you are looking for.</p>
+                    <Link to={Routes.Root} className="linkBtn">Go to Homepage</Link>
+                <img src="/assets/404.webp" alt="404 img" height="300"/>
             </div>
         </>
     );
