f240ccb241d7ad9e73a2760d63f5d53b738347a3
YarnoVandenplas <Yarno.Vandenplas@student.ehb.be>
2026-06-11 15:42:18 +0200

    footer desktop ready

--- Frontend/src/components/footer/Footer.jsx
+++ Frontend/src/components/footer/Footer.jsx
@@ -1,50 +1,64 @@
-import {Link, NavLink} from "react-router";
+import { Link, NavLink } from "react-router";
 import Routes from "../../routes/constants/Routes.js";
 import SocialLinks from "../socialLinks/SocialLinks.jsx";
 import s from "./Footer.module.css";
-import {FooterRoutes} from "../../routes/FooterRoutes.js";
+import { FooterRoutes } from "../../routes/FooterRoutes.js";
 
 export default function Footer() {
-    return (
-        <footer>
-            <div className={`ctx ${s.ctx}`}>
-                <a href="https://www.erasmushogeschool.be/nl/evenementen/shiftfestival" className="linkBtn">
-                    Schrijf je nu gratis in
-                </a>
-                <br/>
-                <div className={s.textInfo}>
-                    <p>
-                        Erasmushogeschool Brussel <br/>
-                        Campus Kaai
-                        <br/>
-                        Nijverheidskaai 170 <br/>
-                        1070 Anderlecht
-                    </p>
-                    <div>
-                        <ul>
-                            {
-                                FooterRoutes.map((link) => (<li key={link.route}>
-                                    <NavLink
-                                        to={link.route}
-                                        className={({isActive}) => `${s.navItem} ${isActive ? s.active : ''}`}
-                                    >
-                                        {link.label}
-                                    </NavLink>
-                                </li>))
-                            }
-                        </ul>
-                    </div>
-                </div>
-                <SocialLinks className={s.socialLinks}/>
-                <small>
-                    Jouw data blijft van jou. Check het{" "}
-                    <Link to={Routes.Privacy}>Privacybeleid</Link>.
-                </small>
-                <small className="footerCopyright">
-                    2026 SHIFT — Erasmushogeschool Brussel. <br/>
-                    Alle rechten voorbehouden.
-                </small>
-            </div>
-        </footer>
-    );
+	return (
+		<footer>
+			<div className={s.footerLogo}>
+				<img
+					src="../../assets/logos/shift_logo.svg"
+					id={s.heroLogo}
+					fetchPriority="high"
+					alt="SHIFT Logo"
+				/>
+			</div>
+			<div className={`ctx ${s.ctx}`}>
+				<div className={s.textInfo}>
+					<div>
+						<ul>
+							{FooterRoutes.map((link) => (
+								<li key={link.route}>
+									<NavLink
+										to={link.route}
+										className={({ isActive }) =>
+											`${s.navItem} ${isActive ? s.active : ""}`
+										}
+									>
+										{link.label}
+									</NavLink>
+								</li>
+							))}
+						</ul>
+					</div>
+					<p>
+						Erasmushogeschool Brussel
+						<br />
+						Campus Kaai
+						<br />
+						Nijverheidskaai 170
+						<br />
+						1070 Anderlecht
+					</p>
+				</div>
+				<div className={s.footerSection2}>
+					<a
+						href="https://www.erasmushogeschool.be/nl/evenementen/shiftfestival"
+						className="linkBtn"
+					>
+						Schrijf je nu gratis in
+					</a>
+					<SocialLinks className={s.socialLinks} />
+				</div>
+				<small className={s.footerCopyright}>
+					Jouw data blijft van jou. Check het{" "}
+					<Link to={Routes.Privacy}>Privacybeleid</Link>. 2026 SHIFT —
+					Erasmushogeschool Brussel. <br />
+					Alle rechten voorbehouden.
+				</small>
+			</div>
+		</footer>
+	);
 }
