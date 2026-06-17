5e5137cfe49d8b1cdd66516950e9aa0a108c9ab3
NolanLamarque <nolan.lamarque@student.ehb.be>
2026-06-17 16:03:43 +0200

    feat: changed style of popup button

--- Frontend/src/components/afterShow/videoPopup.jsx
+++ Frontend/src/components/afterShow/videoPopup.jsx
@@ -7,9 +7,9 @@ function IframePopup({ src, title, onClose }) {
       onClick={(e) => { if (e.target === e.currentTarget) onClose(); }}
       style={{ position:"fixed", inset:0, background:"rgba(0,0,0,0.5)", display:"flex", alignItems:"center", justifyContent:"center", zIndex:1000 }}>
       <div style={{ background: "#27becc", borderRadius:8, width:"80%", maxWidth:600, overflow:"hidden" }}>
-          <div style={{ display:"flex", justifyContent:"space-between", padding:"10px 14px", borderBottom:"1px solid #eee" }}>
+          <div style={{ display:"flex", justifyContent:"space-between", padding:"10px 14px", borderBottom:"1px solid #eee", alignItems: "center" }}>
           <span>{title}</span>
-          <button onClick={onClose}>&times;</button>
+          <button onClick={onClose} style={{padding: 3, paddingRight: 10, paddingLeft: 10, marginBottom: 5, marginRight: 5 }}>&times;</button>
         </div>
         <iframe src={"https://www.youtube.com/embed/suQst6cwW4A?si=NWIGszS2Pp3b0Jpb"} title={"Aftermovie"} style={{ width:"100%", aspectRatio: 16/9, border:"none", display:"block" }} />
       </div>
