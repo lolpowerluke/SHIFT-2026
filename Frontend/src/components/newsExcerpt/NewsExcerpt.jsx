f41640569b0e350589b4ad78f8ad2ed86bcc7bb2
gjx <joachim.gautama@gmail.com>
2026-06-18 16:41:10 +0200

    style: news page and excerpt component
    # Conflicts:
    #	Frontend/src/pages/news/News.jsx

--- Frontend/src/components/newsExcerpt/NewsExcerpt.jsx
+++ Frontend/src/components/newsExcerpt/NewsExcerpt.jsx
@@ -4,9 +4,9 @@ export default function NewsExcerpt({img, imgTitle, title, excerpt, articleLink}
     return (
         <div className={s.wrapper}>
             <img src={img} alt={imgTitle}/>
-            <div>
-                <h3>{title}</h3>
-                <p>{excerpt}</p>
+            <div className={s.txt}>
+                <h3 className={s.title}>{title}</h3>
+                <p className={s.excerpt}>{excerpt}</p>
                 <a href={articleLink} className="linkBtn" target="_blank" rel="noopener noreferrer">Meer info <img src="/assets/arrow_back.svg" alt={"To the article"} /></a>
             </div>
         </div>
--- Frontend/src/pages/news/News.jsx
+++ Frontend/src/pages/news/News.jsx
@@ -5,7 +5,7 @@ import Fotogallery from "../../components/fotoGallery/FotoGallery"
 const articles = [
     {
         articleLink: "lol",
-        excerpt: "lulz",
+        excerpt: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, cumque cupiditate debitis deleniti dolor, doloremque doloribus et excepturi harum impedit inventore laboriosam minima modi nihil odit quibusdam rerum totam ut. Asperiores atque nostrum perferendis porro vero. Accusamus, ad assumenda ducimus eaque eligendi est nostrum numquam optio. Adipisci, aliquid architecto aut, beatae commodi consequuntur deleniti dicta dolore doloribus ducimus ea enim eum expedita facilis hic labore laborum magnam maiores minima minus nemo nihil nobis nulla placeat quam quas quos sed soluta temporibus ut vitae voluptatem voluptatibus voluptatum. A at consequatur cumque fuga nisi nulla obcaecati temporibus! Aut excepturi mollitia natus sequi.",
         img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%2Fid%2FOIP.-Ho2iOlOCWajrep95f39nwHaHE%3Fpid%3DApi&f=1&ipt=7fbc3a71fb216042c796b3b9d0da62164b5cf552068a21e8618c134e54c9cf1b&ipo=images",
         imgTitle: "lolz",
         title: "fu"
@@ -24,16 +24,22 @@ export default function News() {
                 }
             </div>
             <div className="altBg">
+
                 <div className={`${s.mediaSection} ctx`}>
                     <h2>Een beeld zegt meer dan 1000 woorden</h2>
                     <p>Bekijk enkele sfeerbeelden.</p>
                     <div className={s.galleryLayout}>
                         <Fotogallery/>
                     </div>
-
+                    <p><b>Download alle beelden van SHIFT FESTIVAL 2026.</b></p>
+                    <button style={{display: "flex", flexDirection: "row"}}>Download <img
+                        src="/assets/download_icon.svg" style={{height: "2rem", margin: "0 5px 0 1rem"}}/></button>
                 </div>
             </div>
-        
+
+            <div className={`ctx ${s.voorproefje}`}>
+                {/*    TODO: add video team's vid     */}
+            </div>
         </>
     )
 }
