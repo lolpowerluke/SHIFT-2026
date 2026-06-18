58d4d517abd9e3c6a0598831d62fe00f66bc98a7
gjx <joachim.gautama@gmail.com>
2026-06-18 15:23:26 +0200

    feat: news component

---
+++ Frontend/src/components/newsExcerpt/NewsExcerpt.jsx
@@ -0,0 +1,13 @@
+export default function NewsExcerpt({img, imgTitle, title, excerpt, articleLink}) {
+    return (
+        <div>
+            <img src{img} alt={imgTitle}/>
+            <div>
+                <h3>{title}</h3>
+                <span>{excerpt}</span>
+                <a href={articleLink} className="linkBtn" target="_blank" rel="noopener noreferrer">Meer info <img src="/assets/arrow_back.svg" alt={"To the article"} /></a>
+            </div>
+        </div>
+</div>
+)
+}
