8cfd54cd16db9c03d8f95c51a7ae4dfa6e774f3e
gjx <joachim.gautama@gmail.com>
2026-06-18 15:42:17 +0200

    feat: newsexcerpt basic styling

--- Frontend/src/components/newsExcerpt/NewsExcerpt.jsx
+++ Frontend/src/components/newsExcerpt/NewsExcerpt.jsx
@@ -1,10 +1,12 @@
+import s from './NewsExcerpt.module.css';
+
 export default function NewsExcerpt({img, imgTitle, title, excerpt, articleLink}) {
     return (
-        <div>
-            <img src{img} alt={imgTitle}/>
+        <div className={s.wrapper}>
+            <img src={img} alt={imgTitle}/>
             <div>
                 <h3>{title}</h3>
-                <span>{excerpt}</span>
+                <p>{excerpt}</p>
                 <a href={articleLink} className="linkBtn" target="_blank" rel="noopener noreferrer">Meer info <img src="/assets/arrow_back.svg" alt={"To the article"} /></a>
             </div>
         </div>
--- Frontend/src/pages/news/News.jsx
+++ Frontend/src/pages/news/News.jsx
@@ -1,11 +1,20 @@
 import s from "./news.module.css"
+import NewsExcerpt from "../../components/newsExcerpt/NewsExcerpt.jsx";
+
+const articles = [
+    {articleLink: "lol", excerpt: "lulz", img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%2Fid%2FOIP.-Ho2iOlOCWajrep95f39nwHaHE%3Fpid%3DApi&f=1&ipt=7fbc3a71fb216042c796b3b9d0da62164b5cf552068a21e8618c134e54c9cf1b&ipo=images", imgTitle: "lolz", title: "fu"},
+]
 
 export default function News() {
     return (
         <>
-        <div className={s.newsDiv}>
-            <h1>Pers &<br/>media</h1>
-        </div>
+            <div className={s.newsDiv}>
+                <h1>Pers &<br/>media</h1>
+                {articles.map((a, i) =>
+                    <NewsExcerpt articleLink={a.articleLink} excerpt={a.excerpt} img={a.img} imgTitle={a.imgTitle} title={a.title}/>
+                    )
+                }
+            </div>
         </>
     )
 }
