584b093eee50dfc70ea34db669557996b7197839
YarnoVandenplas <Yarno.Vandenplas@student.ehb.be>
2026-06-18 12:04:14 +0200

    FESTIVAL

--- Frontend/src/pages/info/Info.jsx
+++ Frontend/src/pages/info/Info.jsx
@@ -1,7 +1,7 @@
 import s from "./info.module.css";
 import ClockIcon from "../../components/clock/ClockIcon.jsx";
 import FAQItem from "../../components/FAQItem.jsx";
-import {useState} from "react";
+import { useState } from "react";
 
 const events = [
 	{ time: { hr: 17, min: 0 }, event: "start" },
@@ -13,8 +13,8 @@ const events = [
 
 const faq = [
 	{
-		title: "Is het festival gratis?",
-		answer: <>Ja, SHIFT festival is gratis.</>,
+		title: "Is SHIFT FESTIVAL gratis?",
+		answer: <>Ja, SHIFT FESTIVAL is gratis.</>,
 	},
 	{
 		title: "Hoe schrijf ik mij in?",
@@ -33,8 +33,9 @@ const faq = [
 		),
 	},
 	{
-		title: "Voor wie is SHIFT bedoeld?",
-		answer: "SHIFT is bedoeld voor toekomstige studenten die een studiekeuze willen maken, alumni, bedrijven en ouders, familie en vrienden van de derdejaarsstudenten.",
+		title: "Voor wie is SHIFT FESTIVAL bedoeld?",
+		answer:
+			"SHIFT FESTIVAL is bedoeld voor toekomstige studenten die een studiekeuze willen maken, alumni, bedrijven en ouders, familie en vrienden van de derdejaarsstudenten.",
 	},
 	{
 		title: "Is de locatie toegankelijk voor rolstoelgebruikers?",
@@ -47,112 +48,167 @@ const faq = [
 ];
 
 export default function Info() {
-    const [isProgrammaOpen, setIsProgrammaOpen] = useState(false);
-    const [isHowOpen, setIsHowOpen] = useState(false);
+	const [isProgrammaOpen, setIsProgrammaOpen] = useState(false);
+	const [isHowOpen, setIsHowOpen] = useState(false);
 
-    const handleProgrammaClick = () => {
-        setIsProgrammaOpen(!isProgrammaOpen);
-    }
+	const handleProgrammaClick = () => {
+		setIsProgrammaOpen(!isProgrammaOpen);
+	};
 
-    const handleHowClick = () => {
-        setIsHowOpen(!isHowOpen);
-    }
-    return (
-        <>
-            <div className={`${s.heroLayout}`}>
-                <img
-                    src="/assets/icons/LocationOrange.svg"
-                    alt=""
-                    aria-hidden="true"
-                    className={s.locationIcon}
-                />
-                <div className={s.heroTitle}>
-                    <div>Praktische Informatie</div>
-                </div>
-                <a href="#programma" className={`linkBtn ${s.heroBtn} ${s.mobile}`}>
-                    Bekijk het programma
-                </a>
-            </div>
+	const handleHowClick = () => {
+		setIsHowOpen(!isHowOpen);
+	};
+	return (
+		<>
+			<div className={`${s.heroLayout}`}>
+				<img
+					src="/assets/icons/LocationOrange.svg"
+					alt=""
+					aria-hidden="true"
+					className={s.locationIcon}
+				/>
+				<div className={s.heroTitle}>
+					<div>Praktische Informatie</div>
+				</div>
+				<a href="#programma" className={`linkBtn ${s.heroBtn} ${s.mobile}`}>
+					Bekijk het programma
+				</a>
+			</div>
 
-            <div className="altBg">
-                <div className="ctx">
-                    <div className={`${s.infoSection}`}>
-                        <div className={`${s.infoCard}`}>
-                            <h3>Waar</h3>
-                            <h4>Erasmushogeschool Brussel</h4>
-                            <p>Campus Kaai<br/>
-                                Nijverheidskaai 170, 1070 Anderlecht</p>
-                            <iframe
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10077.60824291396!2d4.312529743524132!3d50.84223854859938!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3c40f19faf0f9%3A0x4ef5b683135ecb1e!2sErasmushogeschool%20Brussel!5e0!3m2!1snl!2sbe!4v1781275871945!5m2!1snl!2sbe"
-                                allowFullScreen="false" referrerPolicy="no-referrer"/>
-                            <a className="linkBtn" href="https://maps.app.goo.gl/JWGWbtQpvk9Era6p9" target="_blank">Open
-                                in
-                                Google Maps </a>
-                        </div>
-                        <div className={`${s.infoCard}`} id={s.programma} onClick={handleProgrammaClick}>
-                            <h3 className={`${s.faqTitle}`}>
-                                wanneer <span className={`${s.openBtn} ${s.mobile}`}>{isProgrammaOpen ? "-" : "+"}</span>
-                            </h3>
-                            <h4>Vrijdag 19 juni 2026</h4>
-                            <p>17:00 - 21:30</p>
-                            <div className={`${s.calendarWrap} ${isProgrammaOpen ? `${s.open}` : ''}`}>
-                                <div style={{overflow: 'hidden', width: '100%', marginLeft: '0', marginRight: '0'}}>
-                                    <div className={`${s.calendar}`}>
-                                        {events.map((e) => (
-                                            <div className={s.timeSlot}>
-                                                <ClockIcon s={s} hour={e.time.hr} minute={e.time.min}/>
-                                                <span
-                                                    className={s.startTime}>{`${e.time.hr}:${e.time.min.toString().padStart(2, "0")}`}</span>
-                                                <span className={s.event}>{e.event}</span>
-                                            </div>))}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={`${s.infoCard}`} id={s.how} onClick={handleHowClick}>
-                            <h3 className={`${s.faqTitle}`}>
-                                Hoe <span className={`${s.openBtn} ${s.mobile}`}>{isHowOpen ? "-" : "+"}</span>
-                            </h3>
-                            <p>Campus Kaai is vlot bereikbaar met het openbaar vervoer, per fiets, en met de auto.</p>
-                            <div className={`${s.howWrap} ${isHowOpen ? `${s.open}` : ''}`}>
-                                <div style={{overflow: 'hidden', width: '100%', marginLeft: '0', marginRight: '0'}}>
-                                    <div className={`${s.flexRow}`}>
-                                        <img src="/assets/icons/openbaarVervoer.svg" alt="public transport"/>
-                                        <div>
-                                            <h4>OPENBAAR VERVOER</h4>
-                                            <p>Metro 5 tot Aumale of tram 81 tot Conscience.<br/>Beide op 8 minuten
-                                                wandelen.
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <div className={`${s.flexRow}`}>
-                                        <img src="/assets/icons/fiets.svg" alt="fiets"/>
-                                        <div>
-                                            <h4>MET DE FIETS</h4>
-                                            <p>Veilige fietsenstalling aanwezig aan de hoofdingang van Campus Kaai.</p>
-                                        </div>
-
-                                    </div>
-                                    <div className={`${s.flexRow}`}>
-                                        <img src="/assets/icons/auto.svg" alt="auto"/>
-                                        <div>
-                                            <h4>MET DE AUTO</h4>
-                                            <p>Beperkt parkeren op de campus. Gratis bezoekersparking in de buurt.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="ctx">
-                <h2>FAQ</h2>
-                <div className={`${s.faq}`}>
-                    {faq.map((q, index) => (
-                        <FAQItem key={index} title={q.title} answer={q.answer} s={s}/>
-                    ))}
-                </div>
-            </div>
-        </>);
+			<div className="altBg">
+				<div className="ctx">
+					<div className={`${s.infoSection}`}>
+						<div className={`${s.infoCard}`}>
+							<h3>Waar</h3>
+							<h4>Erasmushogeschool Brussel</h4>
+							<p>
+								Campus Kaai
+								<br />
+								Nijverheidskaai 170, 1070 Anderlecht
+							</p>
+							<iframe
+								src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10077.60824291396!2d4.312529743524132!3d50.84223854859938!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3c40f19faf0f9%3A0x4ef5b683135ecb1e!2sErasmushogeschool%20Brussel!5e0!3m2!1snl!2sbe!4v1781275871945!5m2!1snl!2sbe"
+								allowFullScreen="false"
+								referrerPolicy="no-referrer"
+							/>
+							<a
+								className="linkBtn"
+								href="https://maps.app.goo.gl/JWGWbtQpvk9Era6p9"
+								target="_blank"
+							>
+								Open in Google Maps{" "}
+							</a>
+						</div>
+						<div
+							className={`${s.infoCard}`}
+							id={s.programma}
+							onClick={handleProgrammaClick}
+						>
+							<h3 className={`${s.faqTitle}`}>
+								wanneer{" "}
+								<span className={`${s.openBtn} ${s.mobile}`}>
+									{isProgrammaOpen ? "-" : "+"}
+								</span>
+							</h3>
+							<h4>Vrijdag 19 juni 2026</h4>
+							<p>17:00 - 21:30</p>
+							<div
+								className={`${s.calendarWrap} ${isProgrammaOpen ? `${s.open}` : ""}`}
+							>
+								<div
+									style={{
+										overflow: "hidden",
+										width: "100%",
+										marginLeft: "0",
+										marginRight: "0",
+									}}
+								>
+									<div className={`${s.calendar}`}>
+										{events.map((e) => (
+											<div className={s.timeSlot}>
+												<ClockIcon s={s} hour={e.time.hr} minute={e.time.min} />
+												<span
+													className={s.startTime}
+												>{`${e.time.hr}:${e.time.min.toString().padStart(2, "0")}`}</span>
+												<span className={s.event}>{e.event}</span>
+											</div>
+										))}
+									</div>
+								</div>
+							</div>
+						</div>
+						<div
+							className={`${s.infoCard}`}
+							id={s.how}
+							onClick={handleHowClick}
+						>
+							<h3 className={`${s.faqTitle}`}>
+								Hoe{" "}
+								<span className={`${s.openBtn} ${s.mobile}`}>
+									{isHowOpen ? "-" : "+"}
+								</span>
+							</h3>
+							<p>
+								Campus Kaai is vlot bereikbaar met het openbaar vervoer, per
+								fiets, en met de auto.
+							</p>
+							<div className={`${s.howWrap} ${isHowOpen ? `${s.open}` : ""}`}>
+								<div
+									style={{
+										overflow: "hidden",
+										width: "100%",
+										marginLeft: "0",
+										marginRight: "0",
+									}}
+								>
+									<div className={`${s.flexRow}`}>
+										<img
+											src="/assets/icons/openbaarVervoer.svg"
+											alt="public transport"
+										/>
+										<div>
+											<h4>OPENBAAR VERVOER</h4>
+											<p>
+												Metro 5 tot Aumale of tram 81 tot Conscience.
+												<br />
+												Beide op 8 minuten wandelen.
+											</p>
+										</div>
+									</div>
+									<div className={`${s.flexRow}`}>
+										<img src="/assets/icons/fiets.svg" alt="fiets" />
+										<div>
+											<h4>MET DE FIETS</h4>
+											<p>
+												Veilige fietsenstalling aanwezig aan de hoofdingang van
+												Campus Kaai.
+											</p>
+										</div>
+									</div>
+									<div className={`${s.flexRow}`}>
+										<img src="/assets/icons/auto.svg" alt="auto" />
+										<div>
+											<h4>MET DE AUTO</h4>
+											<p>
+												Beperkt parkeren op de campus. Gratis bezoekersparking
+												in de buurt.
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="ctx">
+				<h2>FAQ</h2>
+				<div className={`${s.faq}`}>
+					{faq.map((q, index) => (
+						<FAQItem key={index} title={q.title} answer={q.answer} s={s} />
+					))}
+				</div>
+			</div>
+		</>
+	);
 }
