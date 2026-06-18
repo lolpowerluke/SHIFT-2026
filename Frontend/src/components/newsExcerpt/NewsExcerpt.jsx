b1313f4b9008d01e2872aabcb16103a182268309
gjx <joachim.gautama@gmail.com>
2026-06-18 15:23:56 +0200

    fix: hotfix

--- Frontend/src/components/newsExcerpt/NewsExcerpt.jsx
+++ Frontend/src/components/newsExcerpt/NewsExcerpt.jsx
@@ -8,6 +8,5 @@ export default function NewsExcerpt({img, imgTitle, title, excerpt, articleLink}
                 <a href={articleLink} className="linkBtn" target="_blank" rel="noopener noreferrer">Meer info <img src="/assets/arrow_back.svg" alt={"To the article"} /></a>
             </div>
         </div>
-</div>
 )
 }
