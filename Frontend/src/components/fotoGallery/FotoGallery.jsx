3762acdf01f7266c5a5c39e5130436453fca3bc2
NolanLamarque <nolan.lamarque@student.ehb.be>
2026-06-18 17:37:20 +0200

    feat: download works for gallery

--- Frontend/src/components/fotoGallery/FotoGallery.jsx
+++ Frontend/src/components/fotoGallery/FotoGallery.jsx
@@ -1,16 +1,24 @@
 import s from "./FotoGallery.module.css";
 
 const defaultItems = [
-    { src: "/assets/gallery_image.jpg", alt: "Gallery image 1" },
-    { src: "/assets/gallery_image.jpg", alt: "Gallery image 2" },
-    { src: "/assets/gallery_image.jpg", alt: "Gallery image 3" },
-    { src: "/assets/gallery_image.jpg", alt: "Gallery image 4" },
-    { src: "/assets/gallery_image.jpg", alt: "Gallery image 5" },
-    { src: "/assets/gallery_image.jpg", alt: "Gallery image 6" },
-    { src: "/assets/gallery_image.jpg", alt: "Gallery image 7" },
-    { src: "/assets/gallery_image.jpg", alt: "Gallery image 8" },
-    { src: "/assets/gallery_image.jpg", alt: "Gallery image 9" },
+    { src: "/assets/gallery_image.jpg", alt: "Gallery image 1", filename: "shift-festival-1.png" },
+    { src: "/assets/gallery_image.jpg", alt: "Gallery image 2", filename: "shift-festival-2.png" },
+    { src: "/assets/gallery_image.jpg", alt: "Gallery image 3", filename: "shift-festival-3.png" },
+    { src: "/assets/gallery_image.jpg", alt: "Gallery image 4", filename: "shift-festival-4.png" },
+    { src: "/assets/heroContent/infoHero.jpg", alt: "Gallery image 5", filename: "shift-festival-5.png" },
+    { src: "/assets/gallery_image.jpg", alt: "Gallery image 6", filename: "shift-festival-6.png" },
+    { src: "/assets/gallery_image.jpg", alt: "Gallery image 7", filename: "shift-festival-7.png" },
+    { src: "/assets/gallery_image.jpg", alt: "Gallery image 8", filename: "shift-festival-8.png" },
+    { src: "/assets/gallery_image.jpg", alt: "Gallery image 9", filename: "shift-festival-9.png" },
 ];
+
+function handleDownload(src, filename) {
+    const a = document.createElement("a");
+    a.href = src;
+    a.download = filename;
+    a.click();
+}
+
 
 export default function Fotogallery({ items = defaultItems }) {
     return (
@@ -20,7 +28,7 @@ export default function Fotogallery({ items = defaultItems }) {
                     <div>
                         <img src={item.src} alt={item.alt} />
                     </div>
-                    <button>
+                    <button onClick={() => handleDownload(item.src, item.filename)}>
                         Download <img src="/assets/BlueDownloadFolderPDF.svg" alt="" />
                     </button>
                 </div>
