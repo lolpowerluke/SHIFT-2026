aa2583e81790c28238b628679dd26ad05c1d9ded
YarnoVandenplas <Yarno.Vandenplas@student.ehb.be>
2026-06-19 21:22:34 +0200

    added the winnersf

--- Frontend/src/components/winnerComponent/WinnerBanner.jsx
+++ Frontend/src/components/winnerComponent/WinnerBanner.jsx
@@ -1,67 +1,122 @@
 import s from "./WinnerBanner.module.css";
-import { } from "react";
-
+import {} from "react";
 
 export default function WinnerBanner() {
-    return (
-        <>
-        <div className={s.cardWrapper}>
-            <h3>Winnaars SHIFT 2026</h3>
-            <div className={s.winCard}>
-                <div className={s.projectCard}>
-                    <p className={s.winTitle}><b>Winnaar impactprijs</b></p>
-                    <div className={s.avatarWrap}>
-                    <img className={s.avatarImg} src="/assets/icons/3de jaars temp.png" alt="profile" />
-                    <img className={s.trophyImg} src="/assets/impact_award.svg" alt="Impact Trophy" />
-                    </div>
-                    <div className={s.awardBox}>
-                        <img src="/assets/award_box.svg" alt="award box" />
-                        <p className={s.awardBoxLabel2}><b>Stijn Bruynbroeck<br/>Qwinte De Valck</b></p>
-                        <p className={s.awardBoxLabel2}>The Last Day In Lhasa</p>
-                    </div>
-                    <a className={s.bekijkBtn}><img src="/assets/icons/arrowLink.svg" alt="arrowLink" /></a>
-                </div>
-                <div className={s.projectCard}>
-                    <p className={s.winTitle}><b>Winnaar innovatieprijs</b></p>
-                    <div className={s.avatarWrap}>
-                    <img className={s.avatarImg} src="/assets/icons/3de jaars temp.png" alt="profile" />
-                    <img className={s.trophyImg} src="/assets/innovative_award.svg" alt="Impact Trophy" />
-                    </div>
-                    <div className={s.awardBox}>
-                        <img src="/assets/award_box.svg" alt="award box" />
-                        <p className={s.awardBoxLabel}><b>Name student</b></p>
-                        <p className={s.awardBoxLabel}>The Last Day In Lhasa</p>
-                    </div>
-                    <a className={s.bekijkBtn}><img src="/assets/icons/arrowLink.svg" alt="arrowLink" /></a>
-                </div>
-                <div className={s.projectCard}>
-                    <p className={s.winTitle}><b>Winnaar juryprijs</b></p>
-                    <div className={s.avatarWrap}>
-                    <img className={s.avatarImg} src="/assets/icons/3de jaars temp.png" alt="profile" />
-                    <img className={s.trophyImg} src="/assets/jury_award.svg" alt="Impact Trophy" />
-                    </div>
-                    <div className={s.awardBox}>
-                        <img src="/assets/award_box.svg" alt="award box" />
-                        <p className={s.awardBoxLabel}><b>Name student</b></p>
-                        <p className={s.awardBoxLabel}>The Last Day In Lhasa</p>
-                    </div>
-                    <a className={s.bekijkBtn}><img src="/assets/icons/arrowLink.svg" alt="arrowLink" /></a>
-                </div>
-                <div className={s.projectCard}>
-                    <p className={s.winTitle}><b>Winnaar publieksprijs</b></p>
-                    <div className={s.avatarWrap}>
-                    <img className={s.avatarImg} src="/assets/icons/3de jaars temp.png" alt="profile" />
-                    <img className={s.trophyImg} src="/assets/public_award.svg" alt="Impact Trophy" />
-                    </div>
-                    <div className={s.awardBox}>
-                        <img src="/assets/award_box.svg" alt="award box" />
-                        <p className={s.awardBoxLabel}><b>Name student</b></p>
-                        <p className={s.awardBoxLabel}>The Last Day In Lhasa</p>
-                    </div>
-                    <a className={s.bekijkBtn}><img src="/assets/icons/arrowLink.svg" alt="arrowLink" /></a>
-                </div>
-            </div>
-        </div>
-        </>
-    );
+	return (
+		<>
+			<div className={s.cardWrapper}>
+				<h3>Winnaars SHIFT 2026</h3>
+				<div className={s.winCard}>
+					<div className={s.projectCard}>
+						<p className={s.winTitle}>
+							<b>Winnaar impactprijs</b>
+						</p>
+						<div className={s.avatarWrap}>
+							<img
+								className={s.avatarImg}
+								src="/assets/ARNO.jpg"
+								alt="profile"
+							/>
+							<img
+								className={s.trophyImg}
+								src="/assets/impact_award.svg"
+								alt="Impact Trophy"
+							/>
+						</div>
+						<div className={s.awardBox}>
+							<img src="/assets/award_box.svg" alt="award box" />
+							<p className={s.awardBoxLabel2}>
+								<b>Arno Baeck</b>
+							</p>
+							<p className={s.awardBoxLabel2}>KAMIL</p>
+						</div>
+						<a className={s.bekijkBtn}>
+							<img src="/assets/icons/arrowLink.svg" alt="arrowLink" />
+						</a>
+					</div>
+					<div className={s.projectCard}>
+						<p className={s.winTitle}>
+							<b>Winnaar innovatieprijs</b>
+						</p>
+						<div className={s.avatarWrap}>
+							<img
+								className={s.avatarImg}
+								src="/assets/MATTEO.webp"
+								alt="profile"
+							/>
+							<img
+								className={s.trophyImg}
+								src="/assets/innovative_award.svg"
+								alt="Impact Trophy"
+							/>
+						</div>
+						<div className={s.awardBox}>
+							<img src="/assets/award_box.svg" alt="award box" />
+							<p className={s.awardBoxLabel}>
+								<b>Metteo Giambarresi</b>
+							</p>
+							<p className={s.awardBoxLabel}>AWAY</p>
+						</div>
+						<a className={s.bekijkBtn}>
+							<img src="/assets/icons/arrowLink.svg" alt="arrowLink" />
+						</a>
+					</div>
+					<div className={s.projectCard}>
+						<p className={s.winTitle}>
+							<b>Winnaar juryprijs</b>
+						</p>
+						<div className={s.avatarWrap}>
+							<img
+								className={s.avatarImg}
+								src="/assets/BEATRICE.webp"
+								alt="profile"
+							/>
+							<img
+								className={s.trophyImg}
+								src="/assets/jury_award.svg"
+								alt="Impact Trophy"
+							/>
+						</div>
+						<div className={s.awardBox}>
+							<img src="/assets/award_box.svg" alt="award box" />
+							<p className={s.awardBoxLabel}>
+								<b>Beatrice Bjoko</b>
+							</p>
+							<p className={s.awardBoxLabel}>LUME</p>
+						</div>
+						<a className={s.bekijkBtn}>
+							<img src="/assets/icons/arrowLink.svg" alt="arrowLink" />
+						</a>
+					</div>
+					<div className={s.projectCard}>
+						<p className={s.winTitle}>
+							<b>Winnaar publieksprijs</b>
+						</p>
+						<div className={s.avatarWrap}>
+							<img
+								className={s.avatarImg}
+								src="/assets/KAORTZ.webp"
+								alt="profile"
+							/>
+							<img
+								className={s.trophyImg}
+								src="/assets/public_award.svg"
+								alt="Impact Trophy"
+							/>
+						</div>
+						<div className={s.awardBox}>
+							<img src="/assets/award_box.svg" alt="award box" />
+							<p className={s.awardBoxLabel}>
+								<b>Sarah El Fraihi</b>
+							</p>
+							<p className={s.awardBoxLabel}>KAORTZ</p>
+						</div>
+						<a className={s.bekijkBtn}>
+							<img src="/assets/icons/arrowLink.svg" alt="arrowLink" />
+						</a>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
